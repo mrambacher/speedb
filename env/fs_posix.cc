176c65d8bf4fecd3b5dc7ba29f963a88e6bfc775
Yuval Ariel <yuval@speedb.io>
2022-05-17 13:34:56 +0300

    SPDB-188 + 221: fs_posix: truncate file on deletion to avoid spikes in latency
    This is especially visible is bitmap-based filesystems, but is probably
    a good thing to do anyway.
    
    Also sleep between file purges to avoid hammering the file system too much.

--- env/fs_posix.cc
+++ env/fs_posix.cc
@@ -663,9 +663,45 @@ class PosixFileSystem : public FileSystem {
 
   IOStatus DeleteFile(const std::string& fname, const IOOptions& /*opts*/,
                       IODebugContext* /*dbg*/) override {
+    int f = open(fname.c_str(), cloexec_flags(O_WRONLY | O_NOFOLLOW, nullptr));
+    if (f < 0) {
+      if (errno != ELOOP) {
+        return IOError("while open() file for unlinking", fname, errno);
+      }
+    }
     IOStatus result;
     if (unlink(fname.c_str()) != 0) {
       result = IOError("while unlink() file", fname, errno);
+    }
+    if (f >= 0) {
+      struct stat statbuf = {};
+      if (fstat(f, &statbuf) == 0 && statbuf.st_nlink == 0) {
+        constexpr size_t kTruncSize = (500 << 20);  // 500 MiB
+        struct timespec sleep_duration = {};
+        sleep_duration.tv_nsec = 10000;  // 10 microseconds
+        for (size_t size = statbuf.st_size; size > kTruncSize;
+             size -= kTruncSize) {
+          if (ftruncate(f, size - kTruncSize) != 0) {
+            break;
+          }
+          // Give the block layer time to catch up before we continue hammering
+          // on
+#if defined(__MACH__)
+          // This doesn't really do what we want, since according to POSIX it
+          // uses CLOCK_REALTIME (which is susceptible to changes backward and
+          // forward by e.g. NTP), but macOS unfortunately doesn't provide sane
+          // access to sleeping with CLOCK_MONOTONIC (SYSTEM_CLOCK in macOS)
+          // and we don't really care if we sleep less because the clock jumps
+          // forward, so we'll just have to hope that it doesn't jump back.
+          nanosleep(&sleep_duration, NULL);
+#else
+          clock_nanosleep(CLOCK_MONOTONIC, 0, &sleep_duration, NULL);
+#endif
+        }
+        // Ignore errors
+        fsync(f);
+      }
+      close(f);
     }
     return result;
   }
